4ad1d1fbe42592cfa22f5b4ead5f140bc432270b
Prakashom18 <shahomprakash097@gmail.com>
2025-12-17 10:31:45 +0545

    Small Project

---
+++ day6/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const fs= require('fs')
+const app = express();
+
+const path = require('path');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname,'public')));
+app.set('views', path.join(__dirname, 'views'));
+
+app.set("view engine","ejs");
+
+app.get('/',function(req,res){
+    fs.readdir(`./files`,function(err,files){
+        res.render('index',{files:files});
+    });
+   
+})
+app.post('/create',function(req,res){
+  fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,function(err){
+    res.redirect('/')
+  })
+})
+app.listen(3000);
