ed4a07911dd0d3919c8f2457ab71eebfbbe7401c
Prakashom18 <shahomprakash097@gmail.com>
2025-12-18 23:44:59 +0545

    Node Js

--- day7/app.js
+++ day7/app.js
@@ -1,17 +1,30 @@
 const express = require('express');
 const path = require('path');
 
+const userModel = require('./userModel');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({extended:true}))
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get('/',function(req,res){
-    res.send('index');
-})
+app.get("/", (req, res) => {
+  res.render("index"); // NOT index.ejs
+});
+
+app.get("/create", async (req, res) => {
+  let createdUser = await userModel.create({
+    name : "harsh",
+    email: "harsh@gmail.com",
+    username:"harsh"
+  })
+  res.send(createdUser);
+});
+
 
 app.listen(3000,function(err){
     console.log("running on port 3000")
