7849734190bf383755712e6c063aad5dde30308f
Prakashom18 <shahomprakash097@gmail.com>
2025-12-18 09:45:20 +0545

    Small Project

--- day6/index.js
+++ day6/index.js
@@ -16,6 +16,20 @@ app.get('/',function(req,res){
     });
    
 })
+
+app.get('/edit/:filename',function(req,res){
+  res.render('edit',{filename:req.params.filename})
+})
+
+app.get('/file/:filename',function(req,res){ 
+  fs.readFile(`./files/${req.params.filename}`,function(err,filedata){
+    res.render('show',{filename: req.params.filename,filedata:filedata});
+
+  })
+   
+})
+
+
 app.post('/create',function(req,res){
   fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`,req.body.details,function(err){
     res.redirect('/')
