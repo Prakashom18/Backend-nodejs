9537c6caa643dcaf45b9348485a7e8a263b0a057
Prakashom18 <shahomprakash097@gmail.com>
2025-12-21 22:58:34 +0545

    Data Association Project

--- day10/app.js
+++ day10/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const path = require('path')
+const userModel = require('./models/userModel');
+const postModel = require('./models/postModel')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
@@ -11,6 +14,14 @@ app.get('/',(req,res)=>{
     res.send('Running homepage');
 })
 
+app.get('/create',async (req,res)=>{
+   let user = await userModel.create({
+        username : "Hari ",
+        age : 25,
+        email : "hari@gmail.com"
+    })
+    res.send(user)
+})
 
 
 app.listen(3000,(err)=>{
