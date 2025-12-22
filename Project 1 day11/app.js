2f8688b3591fb1bd903ffda4c58942fbcb6ca525
Prakashom18 <shahomprakash097@gmail.com>
2025-12-22 10:56:39 +0545

    Project 1-User Blog Website

--- Project 1 day11/app.js
+++ Project 1 day11/app.js
@@ -2,21 +2,31 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const bcrypt = require('bcrypt')
+const cookieParser = require('cookie-parser');
 const userModel = require('./models/userModel')
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(express.static(path.join(__dirname,'public')));
+app.use(cookieParser());
 
 app.set('view engine','ejs')
 
 app.get('/',(req,res)=>{
-    res.send('Homepage');
+    res.render('index');
+})
+
+app.post('/register',(req,res)=>{
+
 })
 
 app.get('/login',(req,res)=>{
     res.send('Login Page')
 })
+
+
+
+
 
 app.listen(3000,(err)=>{
     console.log('running on port 3000');
