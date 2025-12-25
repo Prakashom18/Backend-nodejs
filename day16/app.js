a09a21766e5bb958e25b7c83c8c9a399dfc7958f
Prakashom18 <shahomprakash097@gmail.com>
2025-12-25 23:17:30 +0545

    Multer

--- day16/app.js
+++ day16/app.js
@@ -1,16 +1,40 @@
 const express = require('express');
 const app = express();
+const crypto = require('crypto');
+const path = require('path')
+
+  
+const upload = multer({ storage: storage })
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/images/uploads')
+  },
+  filename: function (req, file, cb) {
+    crypto.randomBytes(12,(err,bytes)=>{
+        const fn = bytes.toString("hex")+ path.extname("abcd.jpeg");
+        cb(null,fn)
+    })
+   
+  }
+})
+
+
 app.set('view engine', 'ejs');
 
 app.get('/',(req,res)=>{
-    res.render('index')
+    res.send('dashboard')
 })
 
-app.get('/upload:id',(req,res)=>{
+app.get('/test',(req,res)=>{
+    res.render('test')
+})
+
+app.post('/upload',(req,res)=>{
+   console.log(req.body)
 
 })
 
